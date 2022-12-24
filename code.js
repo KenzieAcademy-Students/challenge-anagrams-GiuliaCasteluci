4790d9964e34e4d8262156c6568233c89e48cbe8
GiuliaCasteluci <giulia.casteluci@mykenzie.snhu.edu>
2022-12-24 18:30:02 -0300

    --

--- code.js
+++ code.js
@@ -1,8 +1,27 @@
 // Your Code Here.
+// Your Code Here.
 import { words } from "./words.js";
 
 
 
-function anagrams(){
-    
-}
+function anagrams() {
+    var input = words;
+    var abc = input.split(", ");
+
+    for (var i = 0; i < abc.length; i++) {
+
+        var word = abc[i];
+        var alphabetical = word.split("").sort().join("");
+
+        for (var j = 0; j < abc.length; j++) {
+
+            if (i === j) {
+                continue;
+            }
+
+            var other = abc[j];
+            if (alphabetical === other.split("").sort().join("")) {
+                console.log(word + " - " + other + " (" + i + ", " + j + ")");
+            }
+        }
+    };};
